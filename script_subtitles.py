5535ab6183497d121b83cc1dcfd55fb234d990d3
YuriID2 <k.jurijl@gmail.com>
2026-08-30 15:07:53 +0200

    Refactor subtitle script for improved structure

--- script_subtitles.py
+++ script_subtitles.py
@@ -1,39 +1,110 @@
-try:
-   from youtube_transcript_api import YouTubeTranscriptApi
-   import sys
-   import argparse
+import sys
+import argparse
+from urllib.parse import urlparse, parse_qs
 
-   def createParser ():
-       parser = argparse.ArgumentParser()
-       parser.add_argument ('-n', '--name','-u','--url')
-
-       return parser
+from youtube_transcript_api import YouTubeTranscriptApi
 
 
-   if __name__ == '__main__':
-       parser = createParser()
-       namespace = parser.parse_args(sys.argv[1:])
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Получение субтитров YouTube-видео"
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        "-u",
+        "--url",
+        required=True,
+        help="URL YouTube-видео"
+    )
+    return parser
 
-       print ("Получаем титры из видео по адресу, {}!".format (namespace.name) )
+
+def get_video_id(video_url):
+    """Извлекает ID видео из YouTube URL."""
+
+    parsed_url = urlparse(video_url)
+
+    # Обычный URL:
+    # https://www.youtube.com/watch?v=XXXXXXXXXXX
+    if parsed_url.hostname in ("www.youtube.com", "youtube.com", "m.youtube.com"):
+        video_id = parse_qs(parsed_url.query).get("v")
+
+        if video_id:
+            return video_id[0]
+
+    # Короткий URL:
+    # https://youtu.be/XXXXXXXXXXX
+    if parsed_url.hostname == "youtu.be":
+        return parsed_url.path.lstrip("/").split("/")[0]
+
+    raise ValueError(
+        "Не удалось определить ID видео. "
+        "Используй обычную ссылку YouTube, например: "
+        "https://www.youtube.com/watch?v=XXXXXXXXXXX"
+    )
 
 
-       def get_subtitles(video_url):
-           video_id = video_url.split("watch?v=")[1]
-           #имя текстового файла с титрами будет включать id видео
-           subtitles_file='titles_'+video_id+'.txt'
-           transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-           transcript = transcript_list.find_transcript(['ru'])
-           translated_transcript = transcript.fetch()
+def get_subtitles(video_url):
+    video_id = get_video_id(video_url)
 
-           with open(subtitles_file, 'w', encoding='utf-8') as f:
-               for line in translated_transcript:
-                   f.write(line['text'] + '\n')
+    subtitles_file = f"titles_{video_id}.txt"
 
-           print('Всё получилось. Титры записаны в созданный файл: '+subtitles_file)
+    print(f"ID видео: {video_id}")
+    print("Получаем список доступных титров...")
 
-       get_subtitles(namespace.name)
+    api = YouTubeTranscriptApi()
 
-#обернули всё try - except, чтобы выбросить исключение и понять в чём ошибка в случае таковой
-except Exception as err:
-   print('Ничего не получилось, ибо: '+str(err))
+    # Современный API youtube-transcript-api
+    transcript_list = api.list(video_id)
 
+    print("Ищем русские титры...")
+
+    try:
+        # Сначала ищем русские субтитры
+        transcript = transcript_list.find_transcript(["ru"])
+    except Exception:
+        print("Русские титры не найдены. Пробуем английские...")
+
+        try:
+            transcript = transcript_list.find_transcript(["en"])
+        except Exception:
+            raise RuntimeError(
+                "Для этого видео не удалось найти русские или английские титры."
+            )
+
+    print(f"Найдены титры на языке: {transcript.language}")
+
+    translated_transcript = transcript.fetch()
+
+    with open(subtitles_file, "w", encoding="utf-8") as f:
+        for line in translated_transcript:
+            f.write(line.text + "\n")
+
+    print()
+    print("Всё получилось!")
+    print(f"Титры записаны в файл: {subtitles_file}")
+
+
+def main():
+    parser = create_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    print(
+        f"Получаем титры из видео по адресу:\n"
+        f"{namespace.name}\n"
+    )
+
+    get_subtitles(namespace.name)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        print("\nОперация отменена.")
+
+    except Exception as err:
+        print(f"\nНичего не получилось, ибо: {err}")
+       
